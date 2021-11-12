eafe6cd71760b40cd60bfedfe5004999fd5c9d81
SujayM <SujayMane@Hotmail.com>
2021-11-12 18:10:16 +0530

    Add project files.

---
+++ MVC_CORE3_E-Shopping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_CORE3_E_Shopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
