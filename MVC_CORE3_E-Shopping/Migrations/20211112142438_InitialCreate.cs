c5a76b6d4274485fd2f3bd73a5f3b1d8f42ff38c
SujayM <SujayMane@Hotmail.com>
2021-11-13 01:31:27 +0530

    Added code for DB migration - Pages

---
+++ MVC_CORE3_E-Shopping/Migrations/20211112142438_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_CORE3_E_Shopping.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sorting = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pages");
+        }
+    }
+}
--- MVC_CORE3_E-Shopping/Models/Page.cs
+++ MVC_CORE3_E-Shopping/Models/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace MVC_CORE3_E_Shopping.Models
     public class Page
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Slug { get; set; }
+        [Required]
         public string Content { get; set; }
         public int Sorting { get; set; }
     }
