93cda134fa3e0acd09bb8bcd155787bc4976aa1f
SujayM <SujayMane@Hotmail.com>
2021-11-12 19:26:59 +0530

    Added DBContext

---
+++ MVC_CORE3_E-Shopping/Infrastructure/EShoppingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CORE3_E_Shopping.Infrastructure
+{
+    public class EShoppingContext : DbContext
+    {
+        public EShoppingContext(DbContextOptions<EShoppingContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
