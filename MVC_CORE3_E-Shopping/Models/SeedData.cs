05996cf8582d9cababbd727643dd7ba0c0a8b175
SujayM <SujayMane@Hotmail.com>
2021-11-13 01:56:36 +0530

    Added code for SeedData for Pages

---
+++ MVC_CORE3_E-Shopping/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_CORE3_E_Shopping.Infrastructure;
+using System;
+using System.Linq;
+
+namespace MVC_CORE3_E_Shopping.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new EShoppingContext(
+                serviceProvider.GetRequiredService<DbContextOptions<EShoppingContext>>()))
+            {
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+
+                context.Pages.AddRange(
+                        new Page
+                        {
+                            Title = "Home",
+                            Slug = "home",
+                            Content = "home Page",
+                            Sorting = 0
+                        },
+                        new Page
+                        {
+                            Title = "About Us",
+                            Slug = "aboutus",
+                            Content = "About Us Page",
+                            Sorting = 100
+                        },
+                        new Page
+                        {
+                            Title = "Services",
+                            Slug = "services",
+                            Content = "Service Page",
+                            Sorting = 100
+                        },
+                        new Page
+                        {
+                            Title = "Content",
+                            Slug = "content",
+                            Content = "Content Page",
+                            Sorting = 100
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MVC_CORE3_E-Shopping/Program.cs
+++ MVC_CORE3_E-Shopping/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MVC_CORE3_E_Shopping.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,22 @@ namespace MVC_CORE3_E_Shopping
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
