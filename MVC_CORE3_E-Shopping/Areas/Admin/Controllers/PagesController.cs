96761943e0611cd6485dd67c3881eabe9cdf58ff
SujayM <SujayMane@Hotmail.com>
2021-11-13 13:03:23 +0530

    Added Controller and View for Pages

--- MVC_CORE3_E-Shopping/Areas/Admin/Controllers/PagesController.cs
+++ MVC_CORE3_E-Shopping/Areas/Admin/Controllers/PagesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC_CORE3_E_Shopping.Infrastructure;
+using MVC_CORE3_E_Shopping.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,20 @@ namespace MVC_CORE3_E_Shopping.Areas.Admin.Controllers
     [Area("Admin")]
     public class PagesController : Controller
     {
-        
-        public string Index()
+        private readonly EShoppingContext context;
+
+        public PagesController(EShoppingContext context)
         {
-            return "Test";
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }
